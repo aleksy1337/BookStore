36a44b58f3ba6e3c8ea089b1e549bc858fe72622
aleksy1337 <kostyra7578@gmail.com>
2023-01-24 13:46:06 +0100

    changes

---
+++ BookStore1/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using BookStore1.Data;
+using BookStore1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore1.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BookController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Book> books = _context.Books.ToList();
+            return View(books);
+        }
+    }
+}
---
+++ BookStore1/Data/Seed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using BookStore1.Data;
+using BookStore1.Models;
+using System.Diagnostics;
+using System.Net;
+
+namespace BookStore1.Data
+{
+    public class Seed
+    {
+        public static void SeedData(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(new List<Book>()
+                    {
+                        new Book()
+                        {
+                            Title ="Lalka",
+                            Author = "Bolesław Prud",
+
+                            Price = 50
+                        },
+                         new Book()
+                        {
+                            Title = "Pan Tadeusz",
+                            Author = "Adam Mickiewicz",
+                            Price = 50
+                        },
+                    }); ;
+                    context.SaveChanges();
+                }
+                
+                if (!context.Customers.Any())
+                {
+                    context.Customers.AddRange(new List<Customer>()
+                    {
+                        new Customer()
+                        {
+                            Name = "Filip",
+                            Surname = "Copija",
+                            DateOfBirth =new DateTime(2001,01,17),
+                        },
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
+
+    }
+}
